0b7e58d2327c8a645abeb834bdd5e56adf757573
inderraw <inderraw@gmail.com>
2026-07-03 22:59:07 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -33,7 +33,7 @@ resource "azurerm_linux_virtual_machine" "vm" {
   name                = "vm-demo-linux"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
-  size                = "Standard_D2ls_v5"
+  size                = "Standard_D2s_v3"
   admin_username      = "adminuser"
   network_interface_ids = [
     azurerm_network_interface.nic.id,
@@ -50,7 +50,7 @@ resource "azurerm_linux_virtual_machine" "vm" {
   source_image_reference {
     publisher = "Canonical"
     offer     = "0001-com-ubuntu-server-jammy"
-    sku       = "24.04 LTS"
+    sku       = "22_04-lts-gen2"
     version   = "latest"
   }
 }
