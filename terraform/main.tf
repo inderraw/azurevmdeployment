7add19bde408ae5163e5a444b6223e1a7a0f3417
inderraw <inderraw@gmail.com>
2026-07-03 22:53:11 +0530

    Change VM size from Standard_B1s to Standard_D2ls_v5

--- terraform/main.tf
+++ terraform/main.tf
@@ -33,7 +33,7 @@ resource "azurerm_linux_virtual_machine" "vm" {
   name                = "vm-demo-linux"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
-  size                = "Standard_B1s"
+  size                = "Standard_D2ls_v5"
   admin_username      = "adminuser"
   network_interface_ids = [
     azurerm_network_interface.nic.id,
