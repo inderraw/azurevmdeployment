cc40fb7554adc510cb48cd4bcd9c58448b54819e
inderraw <inderraw@gmail.com>
2026-07-03 22:20:08 +0530

    Fix formatting of azurerm provider version

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> v3.117.1"
+      version = "~> 3.117.1"
     }
   }
   # Recommended: Store your state file in an Azure Blob Storage container
